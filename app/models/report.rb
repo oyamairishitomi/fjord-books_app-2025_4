da5b7118e6b6cebc345b7b68a9501a1780307a7c
oyamairishitomi <h.tezuka0707@gmail.com>
2026-05-31 13:23:54 +0900

    add Report Ref

--- app/models/report.rb
+++ app/models/report.rb
@@ -4,6 +4,12 @@ class Report < ApplicationRecord
   belongs_to :user
   has_many :comments, as: :commentable, dependent: :destroy
 
+  has_many :mentioned_report_mentions, class_name: 'ReportMention', foreign_key: :mentioned_report_id, dependent: :destroy, inverse_of: :mentioned_report
+  has_many :mentioned_reports, through: :mentioned_report_mentions, source: :mentioning_report
+
+  has_many :mentioning_report_mentions, class_name: 'ReportMention', foreign_key: :mentioning_report_id, dependent: :destroy, inverse_of: :mentioning_report
+  has_many :mentioning_reports, through: :mentioning_report_mentions, source: :mentioned_report
+
   validates :title, presence: true
   validates :content, presence: true
 
